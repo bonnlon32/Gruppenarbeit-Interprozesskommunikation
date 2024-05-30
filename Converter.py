c4939ed7052e5459e54b495783cfc02a56c2117b
Noah Duevel <noah.red@gmx.de>
2024-05-30 14:58:35 +0200

    Test

---
+++ Converter.py
@@ -0,0 +1,28 @@
+import random
+import time
+#A/D-Converter
+#prüft Messwerte auf Plausibilität (unklar) 
+#konvertiert sie gegebenenfalls (unklar)
+#Synchronisation(!)
+
+def analog_to_digital_converter():
+    #Eingangswert (zufällig zwischen 0-5 Volt)
+    #double
+    analog_value = random.uniform(0, 5)
+    
+    #analoger Wert in einen digitalen Wert (8Bit Auflösung)
+    digital_value = int((analog_value /5)*255)
+    #Binär oder dezimal? (unklar)
+
+    return digital_value
+
+def main():
+    while True:
+        
+        # Ausgabe und Aufruf des Converters
+        print("Digitaler Eingangswert:", analog_to_digital_converter())
+        
+        time.sleep(1)
+
+if _name_ == "_main_":
+    main()
