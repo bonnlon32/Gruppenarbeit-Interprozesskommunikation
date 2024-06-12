814e3793976e49eb514261cac5ff69e590adf80e
Julia <Julia.kerwel@gmail.com>
2024-06-12 20:24:09 +0200

    Pipe öffnen und Summenwert erstellen

--- benannte pipes/stat_process.py
+++ benannte pipes/stat_process.py
@@ -2,8 +2,13 @@ import os
 
 # Erster Entwurf für den Stat-Prozess. Hier wird mit den zuvor erzeugten Zufallszahlen Summe und Durchschnitt berechnet
 def stat_process():
+    pipe_stat = '/tmp/conv_to_stat'  # Pfad zur benannten Pipe für den Stat-Prozess
     pipe_report = '/tmp/stat_to_report'  # Pfad zur benannten Pipe für den Report-Prozess
-    pass
+    
+    fifo_stat = open(pipe_stat, 'r')     # Pipe öffnen
+    
+    summenwert = 0
+    anzahl = 0
 
 if __name__ == "__main__":
     stat_process()
