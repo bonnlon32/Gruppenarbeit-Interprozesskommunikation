b8727c90c657e427cab0b2a780a30519152a1654
Julia <Julia.kerwel@gmail.com>
2024-06-12 18:16:56 +0200

    Erster Entwurf von Log und Report

---
+++ benannte pipes/log.py
@@ -0,0 +1,6 @@
+# Erster Entwurf für den Log-Prozess. Hier werden die Zufallszahlen aus conv in eine Datei gespeichert.
+def log_process():
+    pass
+
+if __name__ == "__main__":
+    log_process()
---
+++ benannte pipes/report.py
@@ -0,0 +1,6 @@
+# Erster Entwurf für den Report-Prozess. Ausgabe der Daten 
+def report_process():
+    pass
+
+if __name__ == "__main__":
+    report_process()
