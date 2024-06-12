0eea844ca67a7d4572326a447fa276e556161768
Julia <Julia.kerwel@gmail.com>
2024-06-12 16:43:33 +0200

    Erstentwurf Conv

---
+++ benannte pipes/conv.py
@@ -0,0 +1,8 @@
+# Erster Entwurf für den Conv-Prozess. Hier werden Zufallszahlen generiert die später weiterverarbeitet werden
+def conv_process():
+    # Diese Funktion wird später Werte verarbeiten
+    pass
+
+if __name__ == "__main__":
+    # Startet den Conv-Prozess
+    conv_process()
