7e5b1497e4d923cbc859a27b2cc1c62486a4f635
Julia <Julia.kerwel@gmail.com>
2024-06-12 16:54:23 +0200

    Pfad für bennate Pipe für conv erstellen

--- benannte pipes/conv.py
+++ benannte pipes/conv.py
@@ -1,7 +1,9 @@
+import os
 # Erster Entwurf für den Conv-Prozess. Hier werden Zufallszahlen generiert die später weiterverarbeitet werden
 def conv_process():
-    # Diese Funktion wird später Werte verarbeiten
-    pass
+     pipe_log = '/tmp/conv_to_log'  # Pfad zur benannten Pipe für den Log-Prozess
+     pipe_stat = '/tmp/conv_to_stat'  # Pfad zur benannten Pipe für den Stat-Prozess
+     pass
 
 if __name__ == "__main__":
     # Startet den Conv-Prozess
