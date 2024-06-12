1d18e50d886e0495a9b4602abcd44ff17df76d38
Julia <Julia.kerwel@gmail.com>
2024-06-12 18:36:08 +0200

    Pfade benannte Pipes report

--- benannte pipes/report.py
+++ benannte pipes/report.py
@@ -1,6 +1,10 @@
+import os
+
 # Erster Entwurf für den Report-Prozess. Ausgabe der Daten 
 def report_process():
-    pass
+        pipe_stat = '/tmp/conv_to_stat'  # Pfad zur benannten Pipe für den Stat-Prozess
+        pipe_report = '/tmp/stat_to_report'  # Pfad zur benannten Pipe für den Report-Prozess
+        pass
 
 if __name__ == "__main__":
     report_process()
