102be274a587f100685be16751a9c22a80f90db1
Julia <Julia.kerwel@gmail.com>
2024-06-12 18:22:30 +0200

    Pfad zur bennanten Pipe für Log erstellt

--- benannte pipes/log.py
+++ benannte pipes/log.py
@@ -1,6 +1,10 @@
+import os
+
 # Erster Entwurf für den Log-Prozess. Hier werden die Zufallszahlen aus conv in eine Datei gespeichert.
 def log_process():
-    pass
+     pipe_log = '/tmp/conv_to_log'  # Der Pfad zur benannten Pipe für den Log-Prozess
+     log_file = 'log.txt'  # Datei, in die die Werte geschrieben werden
+     pass
 
 if __name__ == "__main__":
     log_process()
