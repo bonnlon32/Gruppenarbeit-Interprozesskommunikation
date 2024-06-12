5f3f380a8dbde9546e7acf82f85786a82ddea808
Julia <Julia.kerwel@gmail.com>
2024-06-12 18:43:17 +0200

    Pfad benannte Pipe Report

--- benannte pipes/stat_process.py
+++ benannte pipes/stat_process.py
@@ -1,5 +1,8 @@
+import os
+
 # Erster Entwurf für den Stat-Prozess. Hier wird mit den zuvor erzeugten Zufallszahlen Summe und Durchschnitt berechnet
 def stat_process():
+    pipe_report = '/tmp/stat_to_report'  # Pfad zur benannten Pipe für den Report-Prozess
     pass
 
 if __name__ == "__main__":
