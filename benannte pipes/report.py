9b6afe9ed06d58402bcc8190eb7619fcb8e5a417
Julia <Julia.kerwel@gmail.com>
2024-06-13 15:21:05 +0200

    Endlosschleife Report

--- benannte pipes/report.py
+++ benannte pipes/report.py
@@ -2,9 +2,15 @@ import os
 
 # Erster Entwurf für den Report-Prozess. Ausgabe der Daten 
 def report_process():
-        pipe_stat = '/tmp/conv_to_stat'  # Pfad zur benannten Pipe für den Stat-Prozess
         pipe_report = '/tmp/stat_to_report'  # Pfad zur benannten Pipe für den Report-Prozess
-        pass
+        
+        fifo_report = open(pipe_report, 'r')
+
+while True:
+        daten = fifo_report.readlines()
+        if daten:
+            summenwert, durchschnitt = map(float, daten)
+            print(f"Report: Summe = {summenwert}, Mittelwert = {durchschnitt}")
 
 if __name__ == "__main__":
     report_process()
