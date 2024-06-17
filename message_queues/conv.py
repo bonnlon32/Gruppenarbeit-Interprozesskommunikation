09359b743cee566d420812775124a548fbddbd91
Leon <leon.bonner@t-online.de>
2024-06-17 16:32:30 +0200

    Kleine Änderung im conv Process, da unendliche Schleife nicht mehr nötig
    Implementierung der Main-Methode, sodass conv Werte generiert, woraufhin der Kindprozess Log gestartet wird, der Werte in eine Log-Datei schreibt
    Anmerkung:
    im Log-Process werden noch nicht alle Werte mit aufgezeichnet -> Lösungssuche

--- message_queues/conv.py
+++ message_queues/conv.py
@@ -2,11 +2,10 @@
 import random
 import time
 
-def analog_to_digital_converter():
-    while True:            
-        analog_value = random.uniform(0, 5) #Eingangswert (zufällig zwischen 0-5 Volt)
-        #analoger Wert in einen digitalen Wert (8Bit Auflösung)
-        digital_value = int((analog_value /5)*255)
-        print(digital_value) # Ausgabe auf der Konsole
-        yield digital_value  # Rückgabe der werte mit yield, um einen Generator zu erstellen
-        time.sleep(0.5)  # Wartezeit, um die Werte in Intervallen zu generieren
+def analog_to_digital_converter():         
+    analog_value = random.uniform(0, 5) #Eingangswert (zufällig zwischen 0-5 Volt)
+    #analoger Wert in einen digitalen Wert (8Bit Auflösung)
+    digital_value = int((analog_value /5)*255)
+    print(digital_value) # Ausgabe auf der Konsole
+    time.sleep(0.4)  # Wartezeit, um die Werte in Intervallen zu generieren
+    return digital_value
