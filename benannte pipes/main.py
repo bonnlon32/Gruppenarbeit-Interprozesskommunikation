d5ca7aae82d196d46f1a9effe0d038b3d17d4580
Julia <Julia.kerwel@gmail.com>
2024-06-17 20:18:03 +0200

    Gerüst Main Funktion

---
+++ benannte pipes/main.py
@@ -0,0 +1,6 @@
+# Erstes Gerüst für den Main-Prozess
+def main():
+    pass    # pass ist ein Platzhalter für die spätere Hauptlogik des Programms
+
+if __name__ == "__main__": # Überprüft, ob das Skript direkt ausgeführt wird
+    main()                 # Ruft die Hauptfunktion auf
