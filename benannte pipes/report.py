87534588602ebf00ad861b0d536305d9ec267646
Julia <Julia.kerwel@gmail.com>
2024-06-17 20:09:47 +0200

    Report Verzögerung hinzufügen

--- benannte pipes/report.py
+++ benannte pipes/report.py
@@ -1,16 +1,17 @@
 import os
-
-# Erster Entwurf für den Report-Prozess. 
+import time
+ 
 def report_process():
         pipe_report = '/tmp/stat_to_report'  # Pfad zur benannten Pipe für den Report-Prozess
         
-        fifo_report = open(pipe_report, 'r')
+        while True:                                   # Endlosschleife
+         with open(pipe_report, 'r') as fifo_report:  # Öffnet die benannte Pipe zum lesen
+            daten = fifo_report.readlines()           # Liest alle Werte auf der Pipe aus und speichert sie in der Liste daten
+            if daten:                                 # Prüft, dass die Liste nciht leer ist
+                summenwert, durchschnitt = map(float, daten) # Hier werden die eingelesenen Zahlen in Gleitkommazahlen umgewandelt und sie werden Summenwert und Durchschnitt zugewiesen
+                print(f"Report: Summe = {summenwert}, Mittelwert = {durchschnitt}") # Gibt die Summe und den Durchschnitt auf der Konsole aus
 
-while True:
-        daten = fifo_report.readlines()
-        if daten:
-            summenwert, durchschnitt = map(float, daten)
-            print(f"Report: Summe = {summenwert}, Mittelwert = {durchschnitt}")
+        time.sleep(1)    # Erstellt eine Zeitversögerung von eienr Sekunde
 
 if __name__ == "__main__":
     report_process()
