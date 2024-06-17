5067307ed27725a7927fe6640a72086acbba0193
Julia <Julia.kerwel@gmail.com>
2024-06-17 21:02:40 +0200

    Signalhandler für SIGINT implementieren in der Main

--- benannte pipes/main.py
+++ benannte pipes/main.py
@@ -1,5 +1,7 @@
 import os  
-import signal  # Modul für das Behandeln von Signalen wie SIGINT (Ctrl+C)
+import signal                        # Modul für das Behandeln von Signalen wie SIGINT (Ctrl+C)
+import sys                           # Modul zur Systemmanipulation (z.B. zum Beenden des Programms)
+from multiprocessing import Process  # Modul zum Erstellen und Verwalten von Prozessen
 
 from conv import conv_process          # Importiert die Funktion für den A/D-Wandler-Prozess aus der conv Datei
 from log import log_process            # Importiert die Funktion für den Log-Prozess aus der log Datei
@@ -9,6 +11,12 @@ from report import report_process      # Importiert die Funktion für den Report
 pipe_conv_to_log = '/tmp/conv_to_log'        # Der Pfad für die benannten Pipe für den Log-Prozess
 pipe_conv_to_stat = '/tmp/conv_to_stat'      # Der Pfad für die benannten Pipe für den Stat-Prozess
 pipe_stat_to_report = '/tmp/stat_to_report'  # Der Pfad für die benannten Pipe für den Report-Prozess
+
+# Signalhandler für SIGINT implementieren (Ctrl+C)
+def signal_handler(sig, frame):
+    print("Abbruchsignal empfangen. Prozesse werden beendet...")
+    cleanup()  # Aufruf der Cleanup-Funktion
+    sys.exit(0)  # Beendet das Programm
 
 def main():
     pass    # pass ist ein Platzhalter für die spätere Hauptlogik des Programms
