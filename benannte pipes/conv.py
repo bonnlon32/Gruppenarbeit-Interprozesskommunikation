530e9e4574c5a51803ab48922376483dff6180f0
Julia <Julia.kerwel@gmail.com>
2024-06-18 17:01:38 +0200

    conv, Änderung der Überprüfung ob benannte Pipe existiert

--- benannte pipes/conv.py
+++ benannte pipes/conv.py
@@ -8,31 +8,33 @@ def analog_to_digital_converter():
     analog_value = random.uniform(0, 5)            # Erzeugt einen zufälligen analogen Wert zwischen 0 und 5 Volt.
     digital_value = int((analog_value / 5) * 255)  # Konvertiert den analogen Wert in einen digitalen Wert (8-Bit-Auflösung).
     return digital_value
-#kekse
+
 
 def conv_process():
      pipe_log = '/tmp/conv_to_log'    # Pfad zur benannten Pipe für den Log-Prozess
      pipe_stat = '/tmp/conv_to_stat'  # Pfad zur benannten Pipe für den Stat-Prozess
      
+     if not os.path.exists(pipe_log):  # Überprüft, ob die bannte Pipe schon existiert
+        os.mkfifo(pipe_log)            # Erstellt die benannte Pipe, wenn sie noch nicht existiert
+     if not os.path.exists(pipe_stat): # Überprüft, ob die bannte Pipe schon existiert
+        os.mkfifo(pipe_stat)           # Erstellt die benannte Pipe, wenn sie noch nicht existiert
+
      fifo_log = open(pipe_log, 'w')          # Öffnet die Pipes zum Schreiben (w)
      fifo_stat = open(pipe_stat, 'w')
 
      while True:                              # Endlosschleife erstellen
-      value = analog_to_digital_converter()   # Ruft die Funktion auf um den Wert in die pipes zu schreiben
-      fifo_log.write(f"{value}\n")            # Schreibt die Zahl in die die Pipe mit einem Zeilenumbruch danach
-      fifo_log.flush()                        # Hiermit wird sichergestellt, dass die Zahl sofort in die Pipe geschrieben wird
-      fifo_stat.write(f"{value}\n")
-      fifo_stat.flush()
-      time.sleep(1)                           # Stellt sicher, dass es immer eine Sekunde wartet, bevor eine neue Zahlgeneriert und in die Pipe geschrieben wird
+        value = analog_to_digital_converter()   # Ruft die Funktion auf um den Wert in die pipes zu schreiben
+        fifo_log.write(f"{value}\n")            # Schreibt die Zahl in die die Pipe mit einem Zeilenumbruch danach
+        fifo_log.flush()                        # Hiermit wird sichergestellt, dass die Zahl sofort in die Pipe geschrieben wird
+        fifo_stat.write(f"{value}\n")
+        fifo_stat.flush()
+      
+        time.sleep(1)                           # Stellt sicher, dass es immer eine Sekunde wartet, bevor eine neue Zahlgeneriert und in die Pipe geschrieben wird
 
      fifo_log.close()                        # Schließt die Pipes
      fifo_stat.close()
 
 if __name__ == "__main__":
      
-    if not os.path.exists('/tmp/conv_to_log'):  # Erstellt die benannten Pipe, falls sie nicht existiert
-        os.mkfifo('/tmp/conv_to_log')
-    if not os.path.exists('/tmp/conv_to_stat'): # Erstellt die benannten Pipe, falls sie nicht existiert
-        os.mkfifo('/tmp/conv_to_stat')
     # Startet den Conv-Prozess
     conv_process()
