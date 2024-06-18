d3240cbc60b57d23fb78ef286a1bb63b8eef0837
Julia <Julia.kerwel@gmail.com>
2024-06-18 17:09:35 +0200

    Stat Überprüfung Pipes Existenz

--- benannte pipes/stat_process.py
+++ benannte pipes/stat_process.py
@@ -1,11 +1,15 @@
 import os
 import time
 
-# Erster Entwurf für den Stat-Prozess. Hier wird mit den zuvor erzeugten Zufallszahlen Summe und Durchschnitt berechnet
 def stat_process():
     pipe_stat = '/tmp/conv_to_stat'      # Pfad zur benannten Pipe für den Stat-Prozess
     pipe_report = '/tmp/stat_to_report'  # Pfad zur benannten Pipe für den Report-Prozess
     
+    if not os.path.exists(pipe_stat):
+        os.mkfifo(pipe_stat)
+    if not os.path.exists(pipe_report):
+        os.mkfifo(pipe_report)
+
     fifo_stat = open(pipe_stat, 'r')     # Pipe öffnen
     
     summenwert = 0
@@ -24,7 +28,7 @@ def stat_process():
         with open(pipe_report, 'w') as fifo_report:                       # Pipe zu Report öffnen
                 fifo_report.write(f"{summenwert}\n{durchschnitt}\n")  # Die Summe und den Durchschnitt in die Pipe schreiben
 
-    time.sleep(1)
+        time.sleep(1)
 
     fifo_stat.close()                          #Pipe wird geschlossen
 
