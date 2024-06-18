ef8f5a0fb92afa8675fdddff5ce9d269192e19ab
Noah Duevel <noah.red@gmx.de>
2024-06-18 08:54:06 +0200

    Fehler - durch falsche Person bearbeitet - REUPLOAD

--- message_queues/log.py
+++
@@ -1,4 +0,0 @@
-#log_process
-def write_log_datei(digital_value):  # Generierte Werte aus dem conv process müssen weiter gegeben werden
-    with open("log.txt", "a") as f:  # Erstellen,bzw. öffnen der Datei log.txt im Modus append
-        f.write(f"{digital_value}\n") # Schreiben in die Datei in eine neue Zeile
