531f431ad063f2eef7a9c285c605b669e2ef1c45
Leon <leon.bonner@t-online.de>
2024-06-18 09:45:57 +0200

    Reupload im richtigen Ordner vollständige Implementierung log und conv process mit semaphore und shared memory Optimierung des Codes im conv process zur besseren Verarbeitung

--- shared_memory/conv.py
+++ shared_memory/conv.py
@@ -1,12 +1,11 @@
+#A/D-Converter
 import random
 import time
 
-def analog_to_digital_converter():
-    #Eingangswert (zufällig zwischen 0-5 Volt)
-    #double
-    analog_value = random.uniform(0, 5)
+def analog_to_digital_converter():         
+    analog_value = random.uniform(0, 5) #Eingangswert (zufällig zwischen 0-5 Volt)
     #analoger Wert in einen digitalen Wert (8Bit Auflösung)
     digital_value = int((analog_value /5)*255)
-    #Binär oder dezimal? (unklar)
-    print(digital_value)
+    print(digital_value) # Ausgabe auf der Konsole
+    time.sleep(0.4)  # Wartezeit, um die Werte in Intervallen zu generieren
     return digital_value
