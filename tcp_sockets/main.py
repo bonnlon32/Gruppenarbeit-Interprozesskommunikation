4a45af3eef434ec1cbac5bdfffc63e31d04f75e0
Livia Wischnewski <livia.wischnewski@gmail.com>
2024-06-18 12:53:16 +0200

    first tries of multiprocessing??

--- tcp_sockets/main.py
+++ tcp_sockets/main.py
@@ -18,29 +18,29 @@
 # while True:
 #     pass
 
+#  '# Erstellen und Starten der Prozesse
+# from multiprocessing import Process  # Modul zum Erstellen und Verwalten von Prozessen
 
- # Erstellen und Starten der Prozesse
-from multiprocessing import Process  # Modul zum Erstellen und Verwalten von Prozessen
-
-from conv import conv_process          # Importiert die Funktion für den A/D-Wandler-Prozess aus der conv Datei
-from log import log_process            # Importiert die Funktion für den Log-Prozess aus der log Datei
-from stats import stat_process  # Importiert die Funktion für den Stat-Prozess aus der stat Datei
-from report import report_process      # Importiert die Funktion für den Report-Prozess aus der report Datei
+# from conv import conv_process          # Importiert die Funktion für den A/D-Wandler-Prozess aus der conv Datei
+# from log import log_process            # Importiert die Funktion für den Log-Prozess aus der log Datei
+# from stats import stat_process         # Importiert die Funktion für den Stat-Prozess aus der stat Datei
+# from report import report_process      # Importiert die Funktion für den Report-Prozess aus der report Datei
 
 
-def main():
-    processes = [
-        Process(target=conv_process),   # Erstellen des Prozesses für conv_process
-        Process(target=log_process),    # Erstellen des Prozesses für log_process
-        Process(target=stat_process),   # Erstellen des Prozesses für stat_process
-        Process(target=report_process)  # Erstellen des Prozesses für report_process
-    ]
+# def main():
+#     processes = [
+#         Process(target=conv_process),   # Erstellen des Prozesses für conv_process
+#         Process(target=log_process),    # Erstellen des Prozesses für log_process
+#         Process(target=stat_process),   # Erstellen des Prozesses für stat_process
+#         Process(target=report_process)  # Erstellen des Prozesses für report_process
+#     ]
+
     
-    for p in processes:
-        p.start()  # Startet jeden Prozess
+#     for p in processes:
+#         p.start()  # Startet jeden Prozess
     
-    for p in processes:
-        p.join()  # Wartet, bis jeder Prozess beendet ist
+#     for p in processes:
+#         p.join()  # Wartet, bis jeder Prozess beendet ist
 
-if __name__ == "__main__": # Überprüft, ob das Skript direkt ausgeführt wird
-    main()                 # Ruft die Hauptfunktion auf
+# if __name__ == "__main__": # Überprüft, ob das Skript direkt ausgeführt wird
+#     main()                 # Ruft die Hauptfunktion auf
