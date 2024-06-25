51d5ff2ea2ffb710acdd33b3c05afeafda9f5e68
Livia Wischnewski <livia.wischnewski@gmail.com>
2024-06-25 17:57:30 +0200

    comments and last changes

--- tcp_sockets/conv.py
+++ tcp_sockets/conv.py
@@ -3,12 +3,13 @@
 import random #für random zahl
 import time #für 1 sek abstände
 import socket #für erstellung der sockets
-import struct #um strings von bytes in und aus verschiedenen c-strukturen zu packen & zu entpacken
 
 # Konstanten für Host und Ports (wohin?? hier rein oder main?)
 HOST = "localhost" #localhost adresse (lokal auf dem betriebssystem ohne das netzwerk zu verlassen)
 LOG_PORT = 5002
 STAT_PORT = 5003  #beliebige zahl, nur manche adressen sind reserviert
+
+
 
 # Conv-Prozess: Generiert Zufallszahlen und sendet sie an Log und Stat
 def conv_process():
@@ -20,16 +21,20 @@ def conv_process():
      log_socket.connect((HOST, LOG_PORT))
      stat_socket.connect((HOST, STAT_PORT))
     
+    
 
      while True:
-         measuredValue = random.randint(0, 10)  # Zufallszahl als Messwert
+         measuredValue = random.randint(-1, 10)  # Zufallszahl als Messwert
+         if measuredValue < 0:      #unplausible Werte unter 0 umkonvertieren auf 0
+            measuredValue= 0
+            
          # Messwert als String und Zeilenumbruch senden
-         data = str(measuredValue).encode('utf-8')
-         log_socket.sendall(data + b'\n')
-         stat_socket.sendall(data + b'\n')
+         data = str(measuredValue).encode('utf-8') #umschreiben
+         log_socket.sendall(data + b'\n') #verschicken an log 
+         stat_socket.sendall(data + b'\n') #verschicken an stats
 
          time.sleep(1)  # Wartezeit zwischen den Messungen
 
-
+#damit wir den conv_process auch in der main starten können
 if __name__ == '__main__':
     conv_process()
